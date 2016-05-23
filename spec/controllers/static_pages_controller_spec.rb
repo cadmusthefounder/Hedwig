5235ef19fa99f013a6b507448afa66761edfb092
cadmusthefounder <cadmusthefounder@gmail.com>
2016-05-23 17:43:51 +0800

    Buggy Test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, :type => :controller do
+  render_views
 
-  it "should get home" do
-    get :home
-    expect(response).to have_http_status(:success)
+  describe "GET Home" do
+    before(:each) do
+      get :home
+    end
+
+    it "should get home" do
+      expect(response).to have_http_status(:success)
+    end
+##
+  #  it "should have correct title" do
+   #   response.should have_selector("title", content: "Home | Hedwig")
+  #  end
+##
   end
 
 end
