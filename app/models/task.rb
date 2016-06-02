3ba8bc19b96693b92af71d6c442f1cb057035bc7
cadmusthefounder <cadmusthefounder@gmail.com>
2016-06-02 15:15:44 +0800

    Task enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,28 @@
 class Task < ApplicationRecord
+  enum status: [ :brand_new, :accepted, :completed ]
+
   validates :from_address, presence: true
   validates :from_postal_code, length: { is: 6 }
   validates :to_address, presence: true
   validates :to_postal_code, length: { is: 6 }
   validates :price, presence: true
+
+  def brand_new?
+    return self.status == "brand_new"
+  end
+
+  def accepted?
+    return self.status == "accepted"
+  end
+
+  def completed?
+    return self.status == "completed"
+  end
+
+  def change_status(new_status)
+    if Task.statuses.key?(new_status) || Task.statuses.value?(new_status)
+      self.status = new_status
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523054414) do
+ActiveRecord::Schema.define(version: 20160602054827) do
 
   create_table "sessions", force: :cascade do |t|
     t.integer  "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160523054414) do
     t.decimal  "price"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.         "status"
   end
 
   create_table "users", force: :cascade do |t|
