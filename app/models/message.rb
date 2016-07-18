83d57cbaf7312f93715f75bb77ed957c31f5f511
Yihang Ho <hoyihang5@gmail.com>
2016-07-18 08:51:10 +0800

    Touch interest when message is updated

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
-  belongs_to :interest
+  belongs_to :interest, touch: true
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe Message, :type => :model do
   it "should respond to interest" do
     expect(@message).to respond_to :interest
   end
+
+  it "should touch the associated interest object" do
+    user = users(:yihang)
+    courier = users(:other)
+    task = tasks(:first_task)
+    interest = task.interests.create!(user: courier, updated_at: 20.years.ago)
+    message = interest.messages.create!(user: courier, message: ":)")
+    expect(interest.updated_at).to be > 10.years.ago
+  end
 end
