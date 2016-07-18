3453668b6e5883f52f376c5694f573bb0515a514
Yihang Ho <hoyihang5@gmail.com>
2016-07-18 09:05:32 +0800

    Implement Interest.accessible_by

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,4 +2,8 @@ class Interest < ApplicationRecord
   belongs_to :task
   belongs_to :user
   has_many :messages
+
+  scope :accessible_by, ->(user) do
+    where(user: user).or(where(task: user.tasks))
+  end
 end
--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -16,4 +16,29 @@ RSpec.describe Interest, :type => :model do
   it "should respond to messages" do
     expect(@interest).to respond_to :messages
   end
+
+  describe "accessible_by" do
+    it "should return all interests accessible by the specified user" do
+      user = users(:yihang)
+      second_task = tasks(:second_task)
+      user.interested_tasks << second_task
+      user.reload
+      expect(user.interested_tasks).to include second_task
+
+      accessible_interests = Interest.accessible_by(user)
+      user_tasks = user.tasks.includes(:interests)
+      user_tasks_interests_count = user.tasks.map(&:interests).map(&:length).reduce(&:+)
+      expect(accessible_interests.length).to eq (user_tasks_interests_count + user.interests.length)
+
+      user.interests.each do |interest|
+        expect(accessible_interests).to include interest
+      end
+
+      user.tasks.each do |task|
+        task.interests.each do |interest|
+          expect(accessible_interests).to include interest
+        end
+      end
+    end
+  end
 end
