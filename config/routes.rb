ba0b558d5a792ff57e07c922ce2ffb04f40b9f7e
Charlton <cadmusthefounder@gmail.com>
2016-07-19 21:31:25 +0800

    Add User show link

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   get 'faq'             =>  'static_pages#faq'
   get 'terms_of_use'    =>  'static_pages#terms_of_use'
   get 'privacy_policy'  =>  'static_pages#privacy_policy'
+  get 'my_profile'      =>  'users#show'
 
   get '/update_profile', to: 'users#edit', as: :update_profile
   patch '/update_profile', to: 'users#update'
   put '/update_profile', to: 'users#update'
+
+  resources :users
 
   resources :sessions, only: [:new, :create]
   delete '/sessions', to: 'sessions#destroy'
