12bad0a6492db962fdebaf64e4fe7832551147d2
Charlton <cadmusthefounder@gmail.com>
2016-07-20 20:25:38 +0800

    Fix review new form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :ensure_logged_in
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:show]
 
   # GET /reviews
   # GET /reviews.json
@@ -18,10 +18,6 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
-  def edit
-  end
-
   # POST /reviews
   # POST /reviews.json
   def create
@@ -31,30 +27,6 @@ class ReviewsController < ApplicationController
       render 'show'
     else
       render 'new'
-    end
-  end
-
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
-  def destroy
-    @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
