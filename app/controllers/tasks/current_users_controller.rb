f6eb4bbd7286e2cf0b19f428e9cf679e5536c948
Charlton <cadmusthefounder@gmail.com>
2016-07-21 17:55:47 +0800

    Fixed sort/search for My Task

--- app/controllers/tasks/current_users_controller.rb
+++ app/controllers/tasks/current_users_controller.rb
@@ -3,7 +3,14 @@ class Tasks::CurrentUsersController < ApplicationController
 
   def index
     @title = 'My Tasks'
-    @tasks = current_user.tasks.paginate(page: params[:page])
-    render 'tasks/index'
+
+    sort_attribute = params[:sort] || "created_at"
+    sort_direction = params[:direction] || "DESC"
+
+    @tasks = current_user.tasks.order(sort_attribute => sort_direction)
+                               .paginate(:per_page => 5, :page => params[:page])
+    @tasks = @tasks.search(params[:search]) if params[:search]
+
+    render 'static_pages/home' unless logged_in? else render 'tasks/index'
   end
 end
