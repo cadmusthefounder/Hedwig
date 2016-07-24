6aa4eb0ecb11344b720be48e56bba14633131007
Charlton <cadmusthefounder@gmail.com>
2016-07-24 19:19:17 +0800

    Fix sort first then search but not the other way

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,5 @@
 module TasksHelper
   def sortable(column,title, direction)
-    link_to title, :sort => column, :direction => direction
+    link_to title, url_for(params.merge(:sort => column, :direction => direction, :page => nil))
   end
 end
